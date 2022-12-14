9b130f8350a94b44e4c8d218410001da7c79a136
nv <naverstay.me@gmail.com>
2022-12-14 09:05:20 +0100

    scrollbar size

--- src/js/main.js
+++ src/js/main.js
@@ -44,6 +44,34 @@ const isotopOptions = {
   //}
 };
 
+function getScrollbarWidth() {
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.className = 'scroll-size';
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
+
+const getBrowserScrollbarSize = () => {
+  document.documentElement.style.setProperty("--app-scroll-size", `${getScrollbarWidth()}px`);
+}
+
+getBrowserScrollbarSize();
+
 const isJsonString = (str) => {
   try {
     JSON.parse(str);
